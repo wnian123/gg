9ae72ce233ffe7fe38d9b0c66b14f1eb8f99a839
王念 <1194857665@qq.com>
2020-03-19 20:44:58 +0800

    'login4'

--- demo/src/App.js
+++ demo/src/App.js
@@ -7,12 +7,8 @@ function App() {
       <p>路藤甲login</p>
       <p>login</p>
       <p>路人乙注册</p>
-<<<<<<< HEAD
       <h2>haha</h2>
-=======
       <p>login22</p>
->
->>>>>>> fb1c6f0c7c1bfccf51ca7641bc6837308ef6ed04
     </div>
   );
 }
